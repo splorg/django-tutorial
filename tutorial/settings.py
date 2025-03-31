bf9ff0c4198505fb93e10f4d11e3cac2bb84a96f
Gabriel Lima <gabriel.lbs002@gmail.com>
2025-03-31 15:46:29 -0300

    part 2

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
@@ -113,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
